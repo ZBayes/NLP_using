4e8de9f02aea0c1cc9717a8fd030f4e0baa84fb4
ZBayes <chashaozgr@163.com>
2018-11-21 13:03:49 +0800

    model evaluation

--- src/shopping_reviews/doc2vec1/main.py
+++ src/shopping_reviews/doc2vec1/main.py
@@ -1,8 +1,7 @@
 # ---------------------------
 # 通过word2vec取均值的方式进行转化，后续运用机器学习的方式进行计算
 # STAGE: developing
-# TODO: 
-# [ ] 模型评估
+# TODO:
 # [ ] 必要的日志记录
 # [ ] 性能记录整理工具
 # ---------------------------
@@ -13,6 +12,7 @@ import pandas as pd
 import jieba
 from gensim.models.word2vec import Word2Vec
 
+from sklearn.metrics import f1_score, precision_score, recall_score, roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
 from sklearn.svm import SVC
@@ -36,41 +36,64 @@ def buildWordVector(text, size, imdb_w2v):
         vec /= count
     return vec
 
+
 # 超参数
-N_DIM = 300 						# word2vec的数量
-MIN_COUNT = 10 						# 保证出现的词数足够做才进入词典
-w2v_EPOCH = 15 						# w2v的训练迭代次数
+N_DIM = 300                         # word2vec的数量
+MIN_COUNT = 10                      # 保证出现的词数足够做才进入词典
+w2v_EPOCH = 15                      # w2v的训练迭代次数
+
 
 
 # 数据加载
-pos = data_loader("../../../data/shopping_review/pos.xls", file_type = 'excel', import_type = 'pd') 
-neg =data_loader("../../../data/shopping_review/neg.xls", file_type = 'excel', import_type = 'pd') 
+pos = data_loader("../../../data/shopping_review/pos.xls",
+                  file_type='excel', import_type='pd')
+neg = data_loader("../../../data/shopping_review/neg.xls",
+                  file_type='excel', import_type='pd')
+
+
 
 # 分词
 def cw(x): return list(jieba.cut(x))
+
 pos['words'] = pos[0].apply(cw)
 neg['words'] = neg[0].apply(cw)
+
+
 
 # 数据集划分
 y = np.concatenate((np.ones(len(pos)), np.zeros(len(neg))))
 x_train, x_test, y_train, y_test = train_test_split(
-        np.concatenate((pos['words'], neg['words'])), y, test_size=0.2,random_state=20)
+    np.concatenate((pos['words'], neg['words'])), y, test_size=0.2, random_state=20)
 
-# word2vec的操作
+
+
+# word2vec-> mean doc2vec
 imdb_w2v = Word2Vec(size=N_DIM, min_count=MIN_COUNT)
 imdb_w2v.build_vocab(x_train)
 
 imdb_w2v.train(x_train, total_examples=len(x_train), epochs=w2v_EPOCH)
 
 train_vecs = np.concatenate(
-        [buildWordVector(z, N_DIM, imdb_w2v) for z in x_train])
+    [buildWordVector(z, N_DIM, imdb_w2v) for z in x_train])
 test_vecs = np.concatenate(
-        [buildWordVector(z, N_DIM, imdb_w2v) for z in x_test])
+    [buildWordVector(z, N_DIM, imdb_w2v) for z in x_test])
 
-# 分类模型
-clf = SVC(kernel='rbf', verbose=True)
+# 分类模型-ML stage
+clf = SVC(kernel='rbf', verbose=True, probability=True,
+          shrinking=True, max_iter=10)
 clf.fit(train_vecs, y_train)
-# joblib.dump(clf, '../../data/shopping_review/svm_model.pkl')
 
 # 模型评估
-print(clf.score(test_vecs, y_test))
+score = clf.score(test_vecs, y_test)
+y_pred = clf.predict(test_vecs)
+paraResultItem = {}
+paraResultItem['score'] = score
+paraResultItem['precision'] = precision_score(y_test, y_pred)
+paraResultItem['recall'] = recall_score(y_test, y_pred)
+paraResultItem['f1'] = f1_score(y_test, y_pred)
+proba = clf.predict_proba(test_vecs)
+Y_prob = []
+for i in proba:
+    Y_prob.append(i[1])
+paraResultItem['auc'] = roc_auc_score(y_test, Y_prob)
+print(paraResultItem)
